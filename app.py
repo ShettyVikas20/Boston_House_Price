389de6c3723d5d81761aaf8813ea017829e2ed8e
Vikas <shettyvikas03@gmail.com>
2023-03-18 13:04:06 +0530

    Template commit

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def predict():
     final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+    return render_template("home.html",prediction_text="Predicted house price is {}".format(output))
 
 
 
